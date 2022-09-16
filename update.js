2e26d05aa4230218cdd9a903b11c887b929706a8
itachi_neuchixa <69711193+itachineuchixa@users.noreply.github.com>
2022-09-16 20:09:45 +0300

    Update update.js

--- update.js
+++ update.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ati
 // @namespace    https://github.com/itachineuchixa/extension/blob/main/update.js
-// @version      0.7
+// @version      0.8
 // @downloadURL https://raw.githubusercontent.com/itachineuchixa/extension/main/update.js
 // @updateURL  https://raw.githubusercontent.com/itachineuchixa/extension/main/update.js
 // @description  try to take over the world!
