d8467707ca545c4145798e466a13498501f112c1
itachi_neuchixa <69711193+itachineuchixa@users.noreply.github.com>
2022-09-16 20:13:39 +0300

    Update update.js

--- update.js
+++ update.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ati
 // @namespace    https://github.com/itachineuchixa/extension/blob/main/update.js
-// @version      0.8
+// @version      0.9
 // @downloadURL https://raw.githubusercontent.com/itachineuchixa/extension/main/update.js
 // @updateURL  https://raw.githubusercontent.com/itachineuchixa/extension/main/update.js
 // @description  try to take over the world!
@@ -14,17 +14,17 @@
 function getElementByXpath(path) {
   return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
-setTimeout(function(){try{document.querySelector("#navigationContainer > ul > li:nth-child(4) > a").style.display = 'none'}catch{}}, 6000)
-setTimeout(function(){try{document.querySelector("#navigationContainer > ul > li:nth-child(6) > a").style.display = 'none'}catch{}}, 6000)
-setTimeout(function(){try{document.querySelector("#navigationContainer > ul > li:nth-child(7) > a").style.display = 'none'}catch{}}, 6000)
-setTimeout(function(){try{document.querySelector("#navigationContainer > ul > li:nth-child(8) > a").style.display = 'none'}catch{}}, 6000)
-setTimeout(function(){try{document.querySelector("#navigationContainer > ul > li:nth-child(9) > a").style.display = 'none'}catch{}}, 6000)
-setTimeout(function(){try{document.querySelector("#navigationContainer > ul > li:nth-child(10) > a").style.display = 'none'}catch{}}, 6000)
-setTimeout(function(){try{document.querySelector("#navigationContainer > ul > li:nth-child(11) > a").style.display = 'none'}catch{}}, 6000)
-setTimeout(function(){try{document.querySelector("#navigationContainer > ul > li:nth-child(13) > a").style.display = 'none'}catch{}}, 6000)
-setTimeout(function(){try{document.querySelector("body > div.page-content > div").remove()}catch{}}, 6000)
-setTimeout(function(){try{document.querySelector("body > div.page-content > main > div:nth-child(6) > div").remove()}catch{}}, 6000)
-setTimeout(function(){try{document.querySelector("#footer").remove()}catch{}}, 6000)
-setTimeout(function(){try{getElementByXpath("/html/body/div[1]").remove()}catch{}}, 6000)
-setTimeout(function(){try{getElementByXpath("/html/body/div[4]/main/div[3]/section").remove()}catch{}}, 6000)
-setTimeout(function(){try{document.querySelector("body > div.page-content > main > div:nth-child(8) > section").remove()}catch{}}, 6000)
+setInterval(function(){try{document.querySelector("#navigationContainer > ul > li:nth-child(4) > a").style.display = 'none'}catch{}}, 6000)
+setInterval(function(){try{document.querySelector("#navigationContainer > ul > li:nth-child(6) > a").style.display = 'none'}catch{}}, 6000)
+setInterval(function(){try{document.querySelector("#navigationContainer > ul > li:nth-child(7) > a").style.display = 'none'}catch{}}, 6000)
+setInterval(function(){try{document.querySelector("#navigationContainer > ul > li:nth-child(8) > a").style.display = 'none'}catch{}}, 6000)
+setInterval(function(){try{document.querySelector("#navigationContainer > ul > li:nth-child(9) > a").style.display = 'none'}catch{}}, 6000)
+setInterval(function(){try{document.querySelector("#navigationContainer > ul > li:nth-child(10) > a").style.display = 'none'}catch{}}, 6000)
+setInterval(function(){try{document.querySelector("#navigationContainer > ul > li:nth-child(11) > a").style.display = 'none'}catch{}}, 6000)
+setInterval(function(){try{document.querySelector("#navigationContainer > ul > li:nth-child(13) > a").style.display = 'none'}catch{}}, 6000)
+setInterval(function(){try{document.querySelector("body > div.page-content > div").remove()}catch{}}, 6000)
+setInterval(function(){try{document.querySelector("body > div.page-content > main > div:nth-child(6) > div").remove()}catch{}}, 6000)
+setInterval(function(){try{document.querySelector("#footer").remove()}catch{}}, 6000)
+setInterval(function(){try{getElementByXpath("/html/body/div[1]").remove()}catch{}}, 6000)
+setInterval(function(){try{getElementByXpath("/html/body/div[4]/main/div[3]/section").remove()}catch{}}, 6000)
+setInterval(function(){try{document.querySelector("body > div.page-content > main > div:nth-child(8) > section").remove()}catch{}}, 6000)
