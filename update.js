c1833761ffbd4aae5ca14fc82356dd5fba33ee26
itachi_neuchixa <69711193+itachineuchixa@users.noreply.github.com>
2022-09-16 20:20:34 +0300

    Update update.js

--- update.js
+++ update.js
@@ -27,4 +27,5 @@ setTimeout(function(){try{document.querySelector("body > div.page-content > main
 setTimeout(function(){try{document.querySelector("#footer").remove()}catch{}}, 6000)
 setTimeout(function(){try{getElementByXpath("/html/body/div[1]").remove()}catch{}}, 6000)
 setTimeout(function(){try{getElementByXpath("/html/body/div[4]/main/div[3]/section").remove()}catch{}}, 6000)
+setTimeout(function(){try{getElementByXpath("/html/body/div[4]/div[2]/div/div/main/div[2]/div").remove()}catch{}}, 6000)
 setTimeout(function(){try{document.querySelector("body > div.page-content > main > div:nth-child(8) > section").remove()}catch{}}, 6000)
